06417159d751d311bd352ddff55e800de75eda7d
Sabit Ololade <53153510+ololade97@users.noreply.github.com>
2023-03-04 09:38:00 +0100

    Create family.sol

---
+++ family.sol
@@ -0,0 +1,37 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+//Create a family tree
+//containing names, ailment and age of your family members
+
+contract Family {
+
+    address owner1;
+    address owner2;
+
+    struct Familytree {
+        string name;
+        string ailment;
+        uint age;
+        uint number;
+    }
+
+    Familytree [] public familytree;
+
+    constructor() {
+        owner1 = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4; //These are public addresses - not safe
+        owner2 = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
+    }
+
+    function familyStore(string memory _name, string memory _ailment, uint _age) public {
+        require(msg.sender == owner1 || msg.sender == owner2);
+        familytree.push(Familytree(_name, _ailment,  _age, familytree.length));
+    }
+
+// You don't need the below since the Familytree is public above
+/*
+    function seeMember(uint _number) public view returns(string memory name, string memory ailment, uint age) {
+           return(familytree[_number].name, familytree[_number].ailment, familytree[_number].age);
+    }
+*/
+}
