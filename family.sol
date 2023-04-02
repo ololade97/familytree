a0f86bba3c45600432fbaaee967ef1c7575bae21
Sabit Ololade <53153510+ololade97@users.noreply.github.com>
2023-04-02 09:36:50 +0100

    Update family.sol

--- family.sol
+++ family.sol
@@ -6,8 +6,7 @@ pragma solidity ^0.8.17;
 
 contract Family {
 
-    address owner1;
-    address owner2;
+    address owner1 = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4; //These are public addresses - not safe
 
     struct Familytree {
         string name;
@@ -19,14 +18,18 @@ contract Family {
     Familytree [] public familytree;
 
     constructor() {
-        owner1 = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4; //These are public addresses - not safe
-        owner2 = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
+       owner1 = msg.sender; 
+    }
+
+    function getOwner() public view returns(address) {
+        return owner1;
     }
 
     function familyStore(string memory _name, string memory _ailment, uint _age) public {
-        require(msg.sender == owner1 || msg.sender == owner2);
+        require(msg.sender == owner1);
         familytree.push(Familytree(_name, _ailment,  _age, familytree.length));
     }
+}
 
 // You don't need the below since the Familytree is public above
 /*
@@ -34,4 +37,4 @@ contract Family {
            return(familytree[_number].name, familytree[_number].ailment, familytree[_number].age);
     }
 */
-}
+
